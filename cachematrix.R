df0c71d286833fc52124b23b8cfe11a0974de75e
RScholle <renee.lipari@gmail.com>
2015-02-21 17:19:23 -0500

    Wrote code for MakeCacheMatrix and cacheSolve
    Wrote code for MakeCacheMatrix and cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This first function, `makeCacheMatrix` creates a list 
+## containing functions to
+## 1. Sets the value of the matrix
+## 2. Gets the value of the matrix
+## 3. Set the value of the inverse matrix
+## 4. Get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {       ##Sets the value of the matrix
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x       ##Gets the value of the matrix
+  setInverse <- function(solve) m <<- solve   ##Sets the value of the inverse matrix
+  getInverse <- function() m                  ##Gets the value of the inverse matrix
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+
+##The following function calculates the inverse matrix of the special 
+##matrix created in the above function.  Before doing so, it first checks 
+## to see if the inverse matrix has already been calculated. If so, it 
+## `get`s the inverse matrix from the cache and skips the computation.
+##Otherwise, it calculates the inverse of the data and sets the value
+##of the inverse matrix in the cache via the `setinverse`function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  ##The 'if' statement is checking to see if inverse matrix cached already 
+  if(!is.null(m)) {   
+    message("getting cached inverse matrix")
+    return(m)
+  }
+  ##If the inverse the inverse matrix is not already chached, it is 
+  ##then computed and stored
+  data <- x$get()
+  m <- solve(data)
+  x$setInverse(m)
+  m
 }
+
+  
